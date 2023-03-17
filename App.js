3aba2eae8f2e9bbdd030075d784a78a327206581
Muhammad Umer Abbas <muhammadumerabbas2005@gmail.com>
2023-03-17 16:45:54 +0500

    new main component made

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { Wrapper } from './src/infrastructure/components/wrappper/wrapper';
 import { theme } from './src/infrastructure/theme';
+import MainScreen from './src/screens/main.screen';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme} >
       <Wrapper>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MainScreen />
       <StatusBar style="auto" />
       </Wrapper>
       </ThemeProvider>
