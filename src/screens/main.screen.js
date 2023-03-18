a3ed5fb885fc5ecdc2c655067015081a14cbb886
Muhammad Umer Abbas <muhammadumerabbas2005@gmail.com>
2023-03-18 13:14:48 +0500

    forecast data added

--- src/screens/main.screen.js
+++ src/screens/main.screen.js
@@ -1,5 +1,11 @@
-import { ActivityIndicator, ScrollView, View } from "react-native";
-import React, { useContext } from "react";
+import {
+  ActivityIndicator,
+  Animated,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useContext, useRef, useState } from "react";
 import { WeatherContext } from "../services/weatherAPI/WeatherContext/WeatherContext";
 import { Text } from "../infrastructure/components/typography/Text";
 import Header from "./mainScreenComponets/Header";
@@ -7,29 +13,73 @@ import { Wrapper } from "../infrastructure/components/wrappper/wrapper";
 import TempCard from "./mainScreenComponets/TempCard";
 import HourlyTempCard from "./mainScreenComponets/HourlyTempCard";
 import moment from "moment/moment";
+import ForecastCard from "./mainScreenComponets/ForecastCard";
+import { Button } from "react-native-paper";
 
 export default function MainScreen() {
   const { forecastData, isLoading } = useContext(WeatherContext);
+  const [expanded, setExpanded] = useState(false);
+
+  const scrollHeight = useRef(new Animated.Value(0)).current;
+
+  const toggleExpanded = () => {
+    if (expanded) {
+      Animated.timing(scrollHeight, {
+        toValue: 150,
+        duration: 300,
+        useNativeDriver: false,
+      }).start(() => setExpanded(false));
+    } else {
+      Animated.timing(scrollHeight, {
+        toValue: 300, // set to the desired expanded height
+        duration: 300,
+        useNativeDriver: false,
+      }).start(() => setExpanded(true));
+    }
+  };
 
   return (
     <Wrapper>
       {!isLoading && forecastData.timezone ? (
-        <>
+        <ScrollView>
           <Header />
           <TempCard
             temp_c={forecastData.daily.temperature_2m_max[0]}
             feelsLike={forecastData.daily.apparent_temperature_max[0]}
           />
-          <ScrollView horizontal={true} style={{marginHorizontal:20}} >
+          <ScrollView horizontal={true} style={{ marginHorizontal: 20 }}>
             {forecastData.hourly.temperature_2m.slice(0, 24).map((temp, i) => {
               return (
-                <HourlyTempCard temp={temp} time={moment(forecastData.hourly.time[i]).format("HH:00")} key={i} />
+                <HourlyTempCard
+                  temp={temp}
+                  time={moment(forecastData.hourly.time[i]).format("HH:00")}
+                  key={i}
+                />
               );
             })}
           </ScrollView>
-        </>
+          <Animated.ScrollView
+            style={{
+              height: scrollHeight,
+              marginHorizontal: 20,
+            }}
+          >
+           {forecastData.daily.time.map((time,i)=>{
+            return(
+              <ForecastCard date={moment(time).format("DD/MM")} maxTemp={forecastData.daily.temperature_2m_max[i]} minTemp={forecastData.daily.temperature_2m_min[i]} key={i} />
+            )
+           })}
+          </Animated.ScrollView>
+          <Button onPress={toggleExpanded}>
+            <Text>{expanded ? "Show Less" : "Show More"}</Text>
+          </Button>
+        </ScrollView>
       ) : (
-        <ActivityIndicator size={50} animating={true} />
+        <ActivityIndicator
+          size={50}
+          animating={true}
+          style={{ marginTop: 20 }}
+        />
       )}
     </Wrapper>
   );
---
+++ src/screens/mainScreenComponets/ForecastCard.js
@@ -0,0 +1,15 @@
+import { View } from 'react-native'
+import React from 'react'
+import { Text } from '../../infrastructure/components/typography/Text'
+import { Spacer } from '../../infrastructure/components/spacer/spacer'
+
+export default function ForecastCard({date,minTemp,maxTemp}) {
+  return (
+    <View style={{flexDirection:'row',height:50,alignItems:'center'}} >
+      <Text style={{flex:1}} >{date}</Text>
+      <Text>{Math.round(minTemp)}</Text>
+      <Spacer position="left" size="large" />
+      <Text>{Math.round(maxTemp)}</Text>
+    </View>
+  )
+}
